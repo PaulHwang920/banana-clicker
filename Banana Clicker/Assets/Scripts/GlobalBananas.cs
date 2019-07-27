b09c54a0e17ac5ab973d8a07f2c807538893e00d
PaulHwang920 <paul17@vt.edu>
2019-07-27 18:14:45 -0400

    make banana button
    - created button that incremements total score when clicked.
    - created sprite for banana button

---
+++ Banana Clicker/Assets/Scripts/GlobalBananas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalBananas : MonoBehaviour
+{
+    public static int bananaCount;
+    public GameObject bananaDisplay;
+    public int internalBanana;
+
+    private void Update()
+    {
+        internalBanana = bananaCount;
+        bananaDisplay.GetComponent<Text>().text = "Bananas: " + internalBanana;
+    }
+}
