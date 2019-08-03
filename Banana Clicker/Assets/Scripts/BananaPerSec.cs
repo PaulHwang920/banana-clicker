9f0e7847ffd81a91d2210faa706155514d1f0aa3
PaulHwang920 <paul17@vt.edu>
2019-08-02 20:05:19 -0400

    New Outline
    Monkey Generators, click upgrade

---
+++ Banana Clicker/Assets/Scripts/BananaPerSec.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaPerSec : MonoBehaviour
+{
+
+    public UnityEngine.UI.Text bpsDisplay;
+    public MainButtonClick click;
+    public ItemManager[] items;
+
+    private void Start()
+    {
+        StartCoroutine(AutoTick());
+    }
+
+    private void Update()
+    {
+        bpsDisplay.text = getBananaPerSec() + " bananas/sec";
+    }
+
+    public int getBananaPerSec()
+    {
+        int tick = 0;
+        foreach(ItemManager item in items)
+        {
+            tick += item.count * item.bananaPerSecond;
+        }
+        return tick;
+    }
+
+    public void autoBananaPerSec()
+    {
+        click.bananas += getBananaPerSec();
+    }
+
+    IEnumerator AutoTick()
+    {
+        while (true)
+        {
+            autoBananaPerSec();
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
